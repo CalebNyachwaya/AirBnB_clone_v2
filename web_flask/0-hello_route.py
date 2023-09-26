79ec31d26c956a0723f824a73d50ab163fe242ba
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-09-25 22:37:51 -0700

    Create README.md

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python3
+"""Flask framework
+    """
 from flask import Flask
 
 app = Flask(__name__)
 
 # Define a route for the root URL with strict_slashes=False
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello():
     return "Hello HBNB!"
 
 if __name__ == '__main__':
